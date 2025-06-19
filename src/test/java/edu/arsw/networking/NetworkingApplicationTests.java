80ca4a1c3752e88f13aa09c44a03351cf9769f75
Andr3xDev <achavarropp@gmail.com>
2025-06-18 23:32:10 -0500

    add: Initial project

---
+++ src/test/java/edu/arsw/networking/NetworkingApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.arsw.networking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
